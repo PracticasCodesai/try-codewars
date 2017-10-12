3891803caeb87c6346a78cd148e2036e2c0fad91
YonePC <YonePC@YONE>
2017-10-12 15:33:04 +0100

    build slice refactor

--- src/ThreeInOne.js
+++ src/ThreeInOne.js
@@ -1,9 +1,8 @@
 "use strict";
 
 let threeInOne = function (array) {
-    let cloneArray = Object.assign([],array);
     let resultArray = Array.from({length: array.length/3});
-    return resultArray.map((n, index) => cloneArray.splice(0,3)
+    return resultArray.map((n, index) => array.slice(3*index, 3*(index+1))
             .reduce((a,b) => a+b));
 };
 
