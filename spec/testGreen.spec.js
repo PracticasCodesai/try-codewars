8cea6759d9b87c47b95a81411563b1c5fd236316
JonayGodoy <Jonay.Godoy.Reyes@gmail.com>
2017-10-12 09:46:36 +0100

    test import

--- spec/testGreen.spec.js
+++ spec/testGreen.spec.js
@@ -4,10 +4,18 @@ let should = require('chai').should();
 let assert = require('chai').assert;
 let expect = require('chai').expect;
 
+let Calculator = (require("../src/jsTestImport"));
+
 
 describe('Basic Test', () => {
     it("test green",() => {
-    true.should.equal(true);
-});
+        true.should.equal(true);
+    });
+
+    it('import test green', function () {
+        let calculator = new Calculator();
+        let result = calculator.suma(5,7);
+        result.should.equal(12);
+    });
 
 });
---
+++ src/jsTestImport.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let Calculator = function(){
+
+    this.suma = function (a, b) {
+        return a+b;
+    }
+
+};
+
+module.exports = Calculator;
