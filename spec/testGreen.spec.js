ad262145f31250324081b734e824d419adeafd45
JonayGodoy <Jonay.Godoy.Reyes@gmail.com>
2017-10-12 09:37:55 +0100

    first commit, environment ready

---
+++ spec/testGreen.spec.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let should = require('chai').should();
+let assert = require('chai').assert;
+let expect = require('chai').expect;
+
+
+describe('Basic Test', () => {
+    it("test green",() => {
+    true.should.equal(true);
+});
+
+});
